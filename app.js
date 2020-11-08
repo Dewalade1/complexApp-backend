dbce444c68e87387196ef4f6ece3c6a8d311d584
Dewa <adewalade@gmail.com>
2020-11-08 22:44:46 +0000

    chore: integrate sentry issue tracker to app

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
-const express = require("express")
-const app = express()
-const sanitizeHTML = require("sanitize-html")
-const jwt = require("jsonwebtoken")
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+const express = require("express");
+const app = express();
+const sanitizeHTML = require("sanitize-html");
+const jwt = require("jsonwebtoken");
 
-app.use("/", require("./router"))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-const server = require("http").createServer(app)
+app.use("/", require("./router"));
+
+const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
-  pingTimeout: 30000
-})
+  pingTimeout: 30000,
+});
 
-io.on("connection", function(socket) {
-  socket.on("chatFromBrowser", function(data) {
+io.on("connection", function (socket) {
+  socket.on("chatFromBrowser", function (data) {
     try {
-      let user = jwt.verify(data.token, process.env.JWTSECRET)
-      socket.broadcast.emit("chatFromServer", { message: sanitizeHTML(data.message, { allowedTags: [], allowedAttributes: {} }), username: user.username, avatar: user.avatar })
+      let user = jwt.verify(data.token, process.env.JWTSECRET);
+      socket.broadcast.emit("chatFromServer", { message: sanitizeHTML(data.message, { allowedTags: [], allowedAttributes: {} }), username: user.username, avatar: user.avatar });
     } catch (e) {
-      console.log("Not a valid token for chat.")
+      console.log("Not a valid token for chat.");
     }
-  })
-})
+  });
+});
 
-module.exports = server
+Sentry.init({
+  dsn: "https://743f20ee73d24d63a94eeb17263d7b2d@o471166.ingest.sentry.io/5504695",
+
+  tracesSampleRate: 0.8,
+});
+
+module.exports = server;
